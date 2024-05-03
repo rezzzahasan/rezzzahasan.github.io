be5e90e40c30b36dd105b30347e295d7d1aab2eb
rezzzahasan <muhammadrezahasan1173@gmail.com>
2024-05-03 16:55:34 +0800

    menambahkan javascript

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ const menuList = document.getElementById("menu-list");
 menuIcon.addEventListener("click", () => {
   menuList.classList.toggle("hidden");
 });
+
+// Parallax
+$(window).on("load", function () {
+  $(".biography").addClass("view");
+  $(".sub-container").addClass("view");
+});
+
+$(window).scroll(function () {
+  let wScroll = $(this).scrollTop();
+  if (wScroll > $(".project").offset().top - 400) {
+    {
+      $(".project-card").each(function (i) {
+        setTimeout(function () {
+          $(".project-card").eq(i).addClass("view");
+        }, 300 * (i + 1));
+      });
+    }
+  }
+  if (wScroll > $(".certificate-container").offset().top - 400) {
+    $(".certificate-container").addClass("view");
+  }
+});
